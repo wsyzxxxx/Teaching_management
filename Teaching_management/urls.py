3dad918fd0813913c2590df466da38be60d00d22
wzj <437367125@qq.com>
2017-12-10 18:41:35 +0800

    完成了大部分学生界面

--- Teaching_management/urls.py
+++ Teaching_management/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from . import views
 
 urlpatterns = [
+    url(r'^$', views.login),
     url(r'^index/', include('student.urls')),
     url(r'^admin/', admin.site.urls),
 ]
--- student/views.py
+++ student/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+
+def course(request):
+    return render(request, 'student_courses.html')
+
+def ret(request):
+    return render(request, 'login.html')
